2f03056f29541a76652e0f17aea90b8026d6b65a
LounesBrahimi <Lounesnewgs5@gmail.com>
2021-05-30 16:36:56 +0200

    dynamique lemmings qui trouvent la sortie

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 module Main where
 
 import Control.Monad ( unless, foldM )
@@ -6,6 +7,8 @@ import Control.Concurrent (threadDelay)
 
 import Data.Map
 import qualified Data.Map as Map
+
+import qualified Data.Sequence as S
 
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -34,6 +37,8 @@ import Keyboard (Keyboard)
 import qualified Keyboard as K
 
 import Coordonnees
+
+import Lemming
 
 import Mouse (Coordonnee, inMaybe)
 import qualified Mouse as Mo
@@ -86,6 +91,9 @@ main = do
   -- chargement des images
   (tmap, smap) <- loadImage renderer "assets/terre.bmp" tmap smap "terre"
   (tmap, smap) <- loadImage renderer "assets/metal.bmp" tmap smap "metal"
+  (tmap, smap) <- loadImage renderer "assets/porte.bmp" tmap smap "porte"
+  (tmap, smap) <- loadImage renderer "assets/lemming_droite.bmp" tmap smap "lemming_droite"
+  (tmap, smap) <- loadImage renderer "assets/lemming_gauche.bmp" tmap smap "lemming_gauche"
   -- initialisation de l'état du jeu
   let gameState = M.initGameState
   -- initialisation de l'état du clavier
@@ -93,43 +101,78 @@ main = do
   -- initialisation de l'état de la souri
   let mous = Mo.createMouse
   -- lancement de la gameLoop
-  gameLoop 60 renderer tmap smap kbd gameState mous
+  gameLoop 60 renderer tmap smap kbd gameState mous etat1
 
-gameLoop :: (RealFrac a, Show a) => a -> Renderer -> TextureMap -> SpriteMap -> Keyboard -> GameState -> Coordonnee ->IO ()
-gameLoop frameRate renderer tmap smap kbd gameState mous= do
+gameLoop :: (RealFrac a, Show a) => a -> Renderer -> TextureMap -> SpriteMap -> Keyboard -> GameState -> Coordonnee -> Etat -> IO ()
+gameLoop frameRate renderer tmap smap kbd gameState mous etat = do
   startTime <- time
-  events <- pollEvents
-  let kbd' = K.handleEvents events kbd
-  let mo = Mo.handleEvents events
-  if mo /= Nothing && M.personnageClicked (inMaybe mo) (M.gameStateToCoord  gameState) then putStrLn $ "# Touché ! #" <> show (inMaybe mo) else putStr ""
-  clear renderer
+  case tourEtat 0 etat of
+    Right ne -> do
+      events <- pollEvents
+      let kbd' = K.handleEvents events kbd
+      let mo = Mo.handleEvents events
+      if mo /= Nothing && M.personnageClicked (inMaybe mo) (M.gameStateToCoord  gameState) then putStrLn $ "# Touché ! #" <> show (inMaybe mo) else putStr ""
+      clear renderer
+      threadDelay 100000
   --- display background
-  S.displaySprite renderer tmap (SM.fetchSprite (SpriteId "background") smap)
+      S.displaySprite renderer tmap (SM.fetchSprite (SpriteId "background") smap)
   --display la terre
-  let (Niveau.Niveau h l map) = exempleNiveau1
-  let listTerres = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Terre) map
-  displayImage renderer tmap smap "terre" listTerres
+      let (Niveau.Niveau h l map) = exempleNiveau1
+      let listTerres = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Terre) map
+      displayImage renderer tmap smap "terre" listTerres
   --------------------------------------
   --display metal
-  let listmetaux = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Metal ) map
-  displayImage renderer tmap smap "metal" listmetaux
+      let listmetaux = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Metal ) map
+      displayImage renderer tmap smap "metal" listmetaux
   --------------------------------------
+  --display porte d'entree
+      let entree = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Entree  ) map
+      displayImage renderer tmap smap "porte" entree
+  --------------------------------------
+    --display porte de sortie
+      let sortie = Map.keys $ filterWithKey (\k x -> Just x == Just Niveau.Sortie) map
+      displayImage renderer tmap smap "porte" sortie
+  --------------------------------------
+      let (Env h l entites cases) = envE ne
   ---
-  present renderer
-  endTime <- time
-  let refreshTime = endTime - startTime
-  let delayTime = floor ((1.0 / frameRate - refreshTime) * 1000)
-  threadDelay $ delayTime * 1000 -- microseconds
-  endTime <- time
-  let deltaTime = endTime - startTime
+      let lemmingsMap = filterWithKey (\k x -> not $ S.null $ S.filter verifyIfIsLemming x) cases
+      afficheLemmings renderer tmap smap lemmingsMap 0 0
+  ------------------------------------
+      present renderer
+      endTime <- time
+      let refreshTime = endTime - startTime
+      let delayTime = floor ((1.0 / frameRate - refreshTime) * 1000)
+      threadDelay $ delayTime * 1000 -- microseconds
+      endTime <- time
+      let deltaTime = endTime - startTime
   -- putStrLn $ "Delta time: " <> (show (deltaTime * 1000)) <> " (ms)"
   -- putStrLn $ "Frame rate: " <> (show (1 / deltaTime)) <> " (frame/s)"
   --- update du game state
-  let gameState' = M.gameStep gameState kbd' deltaTime
-  unless (K.keypressed KeycodeEscape kbd') (gameLoop frameRate renderer tmap smap kbd' gameState' mous)
+      let gameState' = M.gameStep gameState kbd' deltaTime
+      unless (K.keypressed KeycodeEscape kbd') (gameLoop frameRate renderer tmap smap kbd' gameState' mous ne)
 
 displayImage :: Renderer -> TextureMap -> SpriteMap -> String -> [Coordonnees]  -> IO ()
 displayImage renderer tmap smap id [] = return ()
 displayImage renderer tmap smap id ((C x y):as) = do
   S.displaySprite renderer tmap (S.moveTo (SM.fetchSprite (SpriteId id) smap) (fromIntegral x * 40) (fromIntegral y *40) )
   displayImage renderer tmap smap id as
+
+verifyIfIsLemming :: Entite -> Bool 
+verifyIfIsLemming (Lem _ _) = True
+
+afficheLemmings :: Renderer -> TextureMap -> SpriteMap -> Map Coordonnees  (S.Seq Entite) -> CInt -> CInt -> IO ()
+afficheLemmings renderer tmap smap lemmings transx transy = do
+  let marcheursDroite = Map.keys $ filterWithKey (\k v -> not $ S.null $ S.filter estLemmigDroite v) lemmings
+  let marcheursGauche = Map.keys $ filterWithKey (\k v -> not $ S.null $ S.filter estLemmigGauche v) lemmings
+  displayImage renderer tmap smap "lemming_droite" marcheursDroite
+  displayImage renderer tmap smap "lemming_gauche" marcheursGauche
+
+estLemmigDroite :: Entite -> Bool 
+estLemmigDroite (Lem _ (Tombeur droite _ _)) = True
+estLemmigDroite (Lem _ (Marcheur droite _)) = True
+estLemmigDroite (Lem _ _) = False
+
+estLemmigGauche :: Entite -> Bool 
+estLemmigGauche (Lem _ (Tombeur gauche _ _)) = True
+estLemmigGauche (Lem _ (Marcheur gauche _)) = True
+estLemmigGauche (Lem _ _) = False
